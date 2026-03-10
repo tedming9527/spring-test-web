bee4eae9673654a1cbebd625544101b3307f4d63
tedming9527 <tedming9527@gmail.com>
2026-03-10 23:03:35 +0800

    doc: first commit

---
+++ src/main/java/org/example/springtestweb/SpringTestWebApplication.java
@@ -0,0 +1,13 @@
+package org.example.springtestweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestWebApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringTestWebApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/example/springtestweb/SpringTestWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springtestweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestWebApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

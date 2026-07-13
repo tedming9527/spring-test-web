45f1ac9e063ce55fb2938a2197dd558136ed6cc6
tedming9527 <tedming9527@gmail.com>
2026-07-13 17:23:20 +0800

    feat: 创建categoryMapper模块

---
+++ src/main/java/org/example/springtestweb/category/mapper/CategoryMapper.java
@@ -0,0 +1,7 @@
+package org.example.springtestweb.category.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.example.springtestweb.category.entity.Category;
+
+public interface CategoryMapper extends BaseMapper<Category> {
+}

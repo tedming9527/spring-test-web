1ea7b9233dbfc68deadc5f4ecefd42d15ef44b98
tedming9527 <tedming9527@gmail.com>
2026-08-24 23:50:56 +0800

    feat: 移除冲突的SQL

--- src/main/resources/db/migration/V20260515__add_user_auth_fields.sql
+++
@@ -1,6 +0,0 @@
--- Migration: add_user_auth_fields
--- Adds password_hash, roles, last_login to user table (nullable)
-ALTER TABLE `user`
-ADD COLUMN `password_hash` VARCHAR(255) NULL,
-	ADD COLUMN `roles` VARCHAR(255) NULL,
-	ADD COLUMN `last_login` DATETIME NULL;
